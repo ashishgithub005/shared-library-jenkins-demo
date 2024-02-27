6e09082e369eb464af2cdbb5337cc953ece1c42c
ashish kumar <akumar.mt05@gmail.com>
2024-02-27 11:54:33 +0530

    added

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,6 @@
 def dockerImage(){
     echo "*****************: DOcker build START:***************"
     sh "docker build -t akumar10605/k8s:tag ."
+    sh "docker push"
     echo "*****************: DOcker build END:***************"
 }
